493973d4ee9992b654aa5a0b4638db20d1e40496
Rizky Ramadhan <jmiryas@gmail.com>
2021-11-03 22:01:28 +0700

    Add products images

---
+++ lib/models/product_model.dart
@@ -0,0 +1,131 @@
+import 'package:equatable/equatable.dart';
+
+class ProductModel extends Equatable {
+  final String name;
+  final String category;
+  final int rating;
+  final double price;
+  final String image;
+
+  const ProductModel({
+    required this.name,
+    required this.category,
+    required this.rating,
+    required this.price,
+    required this.image,
+  });
+
+  static List<ProductModel> products = [
+    ProductModel(
+        name: "Rexus ALVA II",
+        category: "Webcam",
+        rating: 4,
+        price: 50000,
+        image: "images/Rexus-ALVA-II.png"),
+    ProductModel(
+        name: "Rexus DAIVA",
+        category: "Keyboard",
+        rating: 4,
+        price: 250000,
+        image: "images/Rexus-DAIVA.jpg"),
+    ProductModel(
+        name: "Rexus Gaming Chair 101",
+        category: "Chair",
+        rating: 4,
+        price: 1250000,
+        image: "images/Rexus-Gaming-Chair-101.jpg"),
+    ProductModel(
+        name: "Rexus Gaming Chair 103",
+        category: "Chair",
+        rating: 4,
+        price: 1350000,
+        image: "images/Rexus-Gaming-Chair-103.jpg"),
+    ProductModel(
+        name: "Rexus Gaming Chair DarkThrones",
+        category: "Chair",
+        rating: 4,
+        price: 1450000,
+        image: "images/Rexus-Gaming-Chair-DarkThrones.jpg"),
+    ProductModel(
+        name: "Rexus Gaming Chair R60",
+        category: "Chair",
+        rating: 4,
+        price: 1150000,
+        image: "images/Rexus-Gaming-Chair-R60.jpg"),
+    ProductModel(
+        name: "Rexus Gaming Chair RGC 111",
+        category: "Chair",
+        rating: 4,
+        price: 1250000,
+        image: "images/Rexus-Gaming-Chair-RGC-111.jpg"),
+    ProductModel(
+        name: "Rexus Legionare MX3",
+        category: "Keyboard",
+        rating: 4,
+        price: 250000,
+        image: "images/Rexus-Legionare-MX3.jpg"),
+    ProductModel(
+        name: "Rexus Legionare MX5",
+        category: "Keyboard",
+        rating: 4,
+        price: 230000,
+        image: "images/Rexus-Legionare-MX5.jpg"),
+    ProductModel(
+        name: "Rexus Legionare MX9",
+        category: "Keyboard",
+        rating: 4,
+        price: 230000,
+        image: "images/Rexus-Legionare-MX9.png"),
+    ProductModel(
+        name: "Rexus Legionare MX20",
+        category: "Keyboard",
+        rating: 4,
+        price: 260000,
+        image: "images/Rexus-Legionare-MX20.jpg"),
+    ProductModel(
+        name: "Rexus S5 Aviator",
+        category: "Mouse",
+        rating: 4,
+        price: 160000,
+        image: "images/Rexus-S5-Aviator.jpg"),
+    ProductModel(
+        name: "Rexus SW RX01",
+        category: "Webcam",
+        rating: 4,
+        price: 360000,
+        image: "images/Rexus-SW-RX01.jpeg"),
+    ProductModel(
+        name: "Rexus Thundervox HX9",
+        category: "Headset",
+        rating: 4,
+        price: 350000,
+        image: "images/Rexus-Thundervox-HX9.jpg"),
+    ProductModel(
+        name: "Rexus Thundervox HX20",
+        category: "Headset",
+        rating: 4,
+        price: 370000,
+        image: "images/Rexus-Thundervox-HX20.jpg"),
+    ProductModel(
+        name: "Rexus Thundervox HX25",
+        category: "Headset",
+        rating: 4,
+        price: 3650000,
+        image: "images/Rexus-Thundervox-HX25.jpg"),
+    ProductModel(
+        name: "Rexus Thundervox HX28",
+        category: "Headset",
+        rating: 4,
+        price: 3750000,
+        image: "images/Rexus-Thundervox-HX28.png"),
+    ProductModel(
+        name: "Rexus Vonix F80",
+        category: "Headset",
+        rating: 4,
+        price: 3450000,
+        image: "images/Rexus-Vonix-F80.png"),
+  ];
+
+  @override
+  List<Object> get props => [name, category, rating, price, image];
+}
